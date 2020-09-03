ce75f77b0aa1f9eaaf04d116a8c9b90203361ec1
Liz Moreno <lilita.moreno@hotmail.com>
2020-09-02 21:42:09 -0300

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
-import React from "react";
-import "./styles.css";
+import React, { useState } from "react";
+import "./App.css";
+import Key from "./Key";
 
-export default function App() {
+const notas = [
+  {
+    nombre: "DO",
+    link: require("./notas/do.wav"),
+    tieneSostenido: true
+  },
+  {
+    nombre: "RE",
+    link: require("./notas/re.wav"),
+    tieneSostenido: true
+  },
+  {
+    nombre: "MI",
+    link: require("./notas/mi.wav")
+  },
+  {
+    nombre: "FA",
+    link: require("./notas/fa.wav"),
+    tieneSostenido: true
+  },
+  {
+    nombre: "SOL",
+    link: require("./notas/sol.wav"),
+    tieneSostenido: true
+  },
+  {
+    nombre: "LA",
+    link: require("./notas/la.wav"),
+    tieneSostenido: true
+  },
+  {
+    nombre: "SI",
+    link: require("./notas/si.wav")
+  }
+];
+
+function App() {
+  const [estaSonando, setEstaSonando] = useState(null);
+
+  const handleClick = (nota) => {
+    setEstaSonando(nota.nombre);
+    const sonido = new Audio(nota.link);
+    sonido.play();
+    setTimeout(() => {
+      setEstaSonando(null);
+    }, 150);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="titulo">
+        <h1>Piano con React</h1>
+        <h2>Hecho por Liz</h2>
+        <div class="esta-sonando">{estaSonando && `suena ${estaSonando}`}</div>
+      </div>
+      <div className="contenedor">
+        {notas.map((nota, i) => (
+          <Key
+            key={i}
+            estaSonando={estaSonando}
+            nota={nota}
+            handleClick={handleClick}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
+export default App;
---
+++ src/Key.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Key = ({ estaSonando, nota, handleClick }) => {
+  return (
+    <>
+      <div
+        className={`nota ${estaSonando === nota.nombre && "suena"}`}
+        onClick={() => handleClick(nota)}
+      >
+        {nota.tieneSostenido && <div className="negra"></div>}
+      </div>
+    </>
+  );
+};
+
+export default Key;
